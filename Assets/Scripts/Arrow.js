96e204842fc3084cabeb496c28c2a47a590bc997
PqBenoit <benoit@monsieurdrive.com>
2014-05-06 11:18:44 +0200

    First scene

---
+++ Assets/Scripts/Arrow.js
@@ -0,0 +1,22 @@
+﻿#pragma strict
+
+public var speed : Vector2 = new Vector2(50,50);
+public var arrow : GameObject;
+public var power : float;
+private var movement : Vector2;
+
+function Start () {
+
+}
+
+function Update () {
+	arrow = GameObject.Find("Arrow");
+	
+
+	var isDown = Input.GetKeyDown(KeyCode.Space);
+
+	if (Input.GetButton("Fire1")) {
+		print(Time.frameCount);
+	}
+	// FAIRE COEFF DE DÉCÉLERATION + ÉQUATION DE LA COURBE EN FONCTION DU TEMPS.
+}
